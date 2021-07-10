93dc165dae15d82df73a0a586bab64a052d54280
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-07-10 19:03:50 +0000

    paddle/etc

--- index.js
+++ index.js
@@ -93,158 +93,105 @@ class Brick {
       this.status = 0;
     }
   }
-}  
+} 
+class Paddle {
+    constructor(canvas) {
+      this.color = '#0095DD';
+      this.width = 75;
+      this.height = 10;
+      this.x = (canvas.width - this.width) / 2;
+    }
+  
+    drawPaddle(canvas, ctx, rightPressed, leftPressed) {
+      if (rightPressed && this.x < canvas.width - this.width) {
+        this.x -= 7;
+      } else if (leftPressed && this.x > 0) {
+        this.x += 7;
+      }
+  
+      ctx.beginPath();
+      ctx.rect(this.x, canvas.height - this.height, this.width, this.height);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+
+
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
-
-let dx = 2;
-
-let dy = -2;
-
-let score = 0;
-let lives = 3;
 
 
-const paddleHeight = 10;
-const paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) / 2;
+const ball = new Ball();
+const paddle = new Paddle();
 
-const brickRowCount = 3;
-const brickColumnCount = 5;
+
+const brickRowCount = 5;
+const brickColumnCount = 3;
 const brickWidth = 75;
 const brickHeight = 20;
 const brickPadding = 10;
 const brickOffsetTop = 30;
 const brickOffsetLeft = 30;
-
 const bricks = [];
 for (let c = 0; c < brickColumnCount; c += 1) {
   bricks[c] = [];
-  for (let r = 0; r < brickRowCount; r+=1) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  for (let r = 0; r < brickRowCount; r += 1) {
+    const brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+    const brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+    bricks[c][r] = new Brick(brickX, brickY, 1);
   }
 }
 
 let rightPressed = false;
 let leftPressed = false;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
+/* EVENT LISTENER FUNCTIONS */
 function mouseMoveHandler(e) {
-    const relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-      paddleX = relativeX - paddleWidth / 2;
-    }
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddle.x = relativeX - paddle.width / 2;
   }
-  
+}
+
 function keyDownHandler(e) {
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-      rightPressed = true;
-    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-      leftPressed = true;
-    }
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
   }
-  
+}
+
 function keyUpHandler(e) {
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-      rightPressed = false;
-    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-      leftPressed = false;
-    }
-  }
-
-function drawBricks() {
-    for (let c = 0; c < brickColumnCount; c += 1) {
-      for (let r = 0; r < brickRowCount; r += 1) {
-        if (bricks[c][r].status === 1) {
-          const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-          const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-          bricks[c][r].x = brickX;
-          bricks[c][r].y = brickY;
-          ctx.beginPath();
-          ctx.rect(brickX, brickY, brickWidth, brickHeight);
-          ctx.fillStyle = '#0095DD';
-          ctx.fill();
-          ctx.closePath();
-        }
-    }
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
   }
 }
 
+/* KEY/MOUSE EVENT LISTENERS */
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
-function drawScore() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
-  ctx.fillText("Score: " + score, 8, 20);
-}
-
-function drawLives() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
-  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
-}
-
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = '#2824KG';
-  ctx.fill();
-  ctx.closePath();
-}
-
-function draw() {
+function renderGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawBricks();
-  drawBall();
-  collisionDetection();
-  drawPaddle();
-  drawScore();
-  drawLives();
+  ball.drawBall(ctx);
+  ball.move(canvas);
 
-  if (y + dy < ballRadius) {
-    dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
-    if (x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
-    } else {
-      lives-=1;
-      if (!lives) {
-        alert('GAME OVER'); // eslint-disable-line no-alert
-        document.location.reload();
-      } else {
-        x = canvas.width / 2;
-        y = canvas.height - 30;
-        dx = 3;
-        dy = -3;
-        paddleX = (canvas.width - paddleWidth) / 2;
+  for (let c = 0; c < brickColumnCount; c += 1) {
+    for (let r = 0; r < brickRowCount; r += 1) {
+      if (bricks[c][r].status === 1) {
+        bricks[c][r].drawBrick(ctx);
+
+        bricks[c][r].detectCollision(ball);
       }
     }
   }
 
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-
-  if (rightPressed) {
-    paddleX += 7;
-    if (paddleX + paddleWidth > canvas.width) {
-      paddleX = canvas.width - paddleWidth;
-    }
-  } else if (leftPressed) {
-    paddleX -= 7;
-    if (paddleX < 0) {
-      paddleX = 0;
-    }
-  }
-
-  x += dx;
-  y += dy;
-  requestAnimationForm(draw); // eslint-disable-line no-undef
+  paddle.drawPaddle(canvas, ctx, leftPressed, rightPressed);
+  ball.determineLoss(canvas, paddle);
 }
-
-draw();
+setInterval(renderGame, 10);
